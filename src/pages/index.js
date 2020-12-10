7a99e8b79ce81e31be871f55cfb7e00e7e637634
goupilAnthony <anthony.93460@gmail.com>
2020-12-10 23:25:44 +0100

    Layout and start header

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+//import { Link } from "gatsby"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <p>seifuhz</p>
+  </Layout>
+)
+
+export default IndexPage
