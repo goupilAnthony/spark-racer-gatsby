6b2de7d5139d1c6af12bf22f3cb8e5a12d5842e9
goupilAnthony <anthony.93460@gmail.com>
2020-12-16 13:01:42 +0100

    config manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,45 @@
 module.exports = {
-  siteMetadata: {
-    title: `Spark Racer`,
-    description: `Le meilleur endroit pour devenir un meilleur pilote.`,
-    author: `@gougou934`,
-    lang: `fr`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Spark Racer`,
+        description: `Le meilleur endroit pour devenir un meilleur pilote.`,
+        author: `@gougou934`,
+        lang: `fr`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Spark Racer`,
-        short_name: `Spark Racer`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Spark Racer`,
+                short_name: `Spark Racer`,
+                description: `Boutique Spark racers`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `standalone`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                theme_color_in_head: false,
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        `gatsby-plugin-offline`,
 
-    'gatsby-plugin-postcss',
-    {
-      resolve: 'gatsby-plugin-snipcartv3',
-      options: {
-        apiKey: process.env.GATSBY_SNIPCART_API_KEY
-      }
-    }
-  ],
+        'gatsby-plugin-postcss',
+        {
+            resolve: 'gatsby-plugin-snipcartv3',
+            options: {
+                apiKey: process.env.GATSBY_SNIPCART_API_KEY
+            }
+        }
+    ],
 }
