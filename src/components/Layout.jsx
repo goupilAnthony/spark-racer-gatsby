a56e2ec6ef2c618bd6dd3611ecd31a20388ae103
goupilAnthony <anthony.93460@gmail.com>
2021-01-04 00:26:03 +0100

    netlify identity widget

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global.css'
 
 const Layout = ({ children }) => {
+
+    useEffect(() => {
+        const netlifyIdentity = require('netlify-identity-widget');
+ 
+        netlifyIdentity.init({
+            container: '#netlify-modal',
+            locale: 'fr', // defaults to 'en'
+        });
+    }, [])
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
             <a className="absolute -top-10 focus:top-0" href="#maincontent">Skip to main</a>
             <main id="maincontent" className="flex-grow flex">
+            <div data-netlify-identity-menu id="netlify-modal"></div>
+
                 {children}
             </main>
             <Footer />
